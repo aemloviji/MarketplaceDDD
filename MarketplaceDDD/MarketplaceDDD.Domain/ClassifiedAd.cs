dc5b9b64cbb60bf1950ee2d3372cfdafa7025e57
Elvin Asadov <aemloviji@gmail.com>
2026-03-29 20:17:19 +0400

    page-105: constraints for input values

--- MarketplaceDDD/MarketplaceDDD.Domain/ClassifiedAd.cs
+++ MarketplaceDDD/MarketplaceDDD.Domain/ClassifiedAd.cs
@@ -6,14 +6,20 @@ namespace MarketplaceDDD.Domain
     {
         public Guid Id { get; }
 
-        public ClassifiedAd(Guid id)
+        public ClassifiedAd(Guid id, Guid ownerId)
         {
             if (id == default)
             {
                 throw new ArgumentException("Identity must be specified", nameof(id));
             }
 
+            if (ownerId == default)
+            {
+                throw new ArgumentException("Owner id must be specified", nameof(ownerId));
+            }
+
             Id = id;
+            _ownerId = ownerId;
         }
 
         public void SetTitle(string title) => _title = title;
