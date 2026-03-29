f6d660f925b3256155ec62dbab9808b5cf02dd86
Elvin Asadov <aemloviji@gmail.com>
2026-03-29 20:30:44 +0400

    page-106: value objects

---
+++ MarketplaceDDD/MarketplaceDDD.Domain/ClassifiedAdId.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MarketplaceDDD.Domain
+{
+    public class ClassifiedAdId
+    {
+        private readonly Guid _value;
+
+        public ClassifiedAdId(Guid value)
+        {
+            if (value == default)
+            {
+                throw new ArgumentNullException(nameof(value), "Classified Ad Id cannot be empty");
+            }
+
+            _value = value;
+        }
+    }
+}
