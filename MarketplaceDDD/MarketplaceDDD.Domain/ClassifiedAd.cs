d90ddc5c57be3adcdb87e9c89c85b2fc9b09cfde
Elvin Asadov <aemloviji@gmail.com>
2026-03-29 20:05:45 +0400

    page-102: classified ad entity

---
+++ MarketplaceDDD/MarketplaceDDD.Domain/ClassifiedAd.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MarketplaceDDD.Domain
+{
+    public class ClassifiedAd
+    {
+        public Guid Id { get; }
+
+        public ClassifiedAd(Guid id)
+        {
+            if (id == default)
+            {
+                throw new ArgumentException("Identity must be specified", nameof(id));
+            }
+
+            Id = id;
+        }
+
+        private Guid _ownerId;
+        private string _title;
+        private string _text;
+        private decimal _price;
+    }
+}
