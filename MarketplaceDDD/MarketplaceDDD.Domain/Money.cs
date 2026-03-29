caa71104c4264b7144f9e46f91d9980c31068c48
Elvin Asadov <aemloviji@gmail.com>
2026-03-29 20:49:51 +0400

    page-109(money): value objects

---
+++ MarketplaceDDD/MarketplaceDDD.Domain/Money.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MarketplaceDDD.Domain
+{
+    public class Money : IEquatable<Money?>
+    {
+        private decimal Amount { get; }
+
+        public Money(decimal amount)
+        {
+            Amount = amount;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return Equals(obj as Money);
+        }
+
+        public bool Equals(Money? other)
+        {
+            return !(other is null) &&
+                   Amount == other.Amount;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Amount);
+        }
+
+        public static bool operator ==(Money? left, Money? right)
+        {
+            return EqualityComparer<Money>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Money? left, Money? right)
+        {
+            return !(left == right);
+        }
+    }
+}
---
+++ MarketplaceDDD/MarketplaceDDD.Tests/MoneyTest.cs
@@ -0,0 +1,17 @@
+﻿using MarketplaceDDD.Domain;
+using Xunit;
+
+namespace MarketplaceDDD.Tests
+{
+    public class MoneyTest
+    {
+        [Fact]
+        public void Money_objects_with_the_same_amount_should_be_equal()
+        {
+            var firstAmount = new Money(5);
+            var secondAmount = new Money(5);
+            Assert.Equal(firstAmount, secondAmount);
+        }
+    }
+
+}
