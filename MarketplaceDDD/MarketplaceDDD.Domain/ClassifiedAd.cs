fb1abd299b129eacc5b7340b5df2e08799aa593a
Elvin Asadov <aemloviji@gmail.com>
2026-04-04 23:19:13 +0400

    page-129: entity invariants

--- MarketplaceDDD/MarketplaceDDD.Domain/ClassifiedAd.cs
+++ MarketplaceDDD/MarketplaceDDD.Domain/ClassifiedAd.cs
@@ -1,26 +1,58 @@
-﻿using System;
-
-namespace MarketplaceDDD.Domain
+﻿namespace MarketplaceDDD.Domain
 {
     public class ClassifiedAd
     {
-        public ClassifiedAdId Id { get; }
+        public ClassifiedAdId Id { get; private set; }
+
+        public UserId OwnerId { get; private set; }
+
+        public ClassifiedAdTitle Title { get; private set; }
+
+        public ClassifiedAdText Text { get; private set; }
+
+        public Price Price { get; private set; }
+
+        public ClassifiedAdState State { get; private set; }
 
         public ClassifiedAd(ClassifiedAdId id, UserId ownerId)
         {
             Id = id;
-            _ownerId = ownerId;
+            OwnerId = ownerId;
+            State = ClassifiedAdState.Inactive;
         }
 
-        public void SetTitle(ClassifiedAdTitle title) => _title = title;
+        public void SetTitle(ClassifiedAdTitle title) => Title = title;
 
-        public void UpdateText(ClassifiedAdText text) => _text = text;
+        public void UpdateText(ClassifiedAdText text) => Text = text;
 
-        public void UpdatePrice(Price price) => _price = price;
+        public void UpdatePrice(Price price) => Price = price;
 
-        private UserId _ownerId;
-        private ClassifiedAdTitle _title;
-        private ClassifiedAdText _text;
-        private Price _price;
+        public void RequestToPublish()
+        {
+            if (Title is null)
+            {
+                throw new InvalidEntityStateException(this, "title cannot be empty");
+            }
+
+            if (Text is null)
+            {
+                throw new InvalidEntityStateException(this, "text cannot be empty");
+            }
+
+            if (Price?.Amount == 0)
+            {
+                throw new InvalidEntityStateException(this, "price cannot be zero");
+            }
+
+            State = ClassifiedAdState.PendingReview;
+        }
+
+        public enum ClassifiedAdState
+        {
+            PendingReview,
+            Active,
+            Inactive,
+            MarkedAsSold
+        }
     }
 }
