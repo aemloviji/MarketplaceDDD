4e27bfbc8789d9e6ba2007822c00e328fde84571
Elvin Asadov <aemloviji@gmail.com>
2026-04-12 18:08:11 +0400

    page-155: http endpoints

---
+++ MarketplaceDDD/MarketplaceDDD/Api/ClassifiedAdsCommandsApi.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace MarketplaceDDD.Api
+{
+    [ApiController]
+    [Route("/ad")]
+    public class ClassifiedAdsCommandsApi : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post(Contracts.ClassifiedAds.V1.Create request)
+        {
+            return Ok();
+        }
+    }
+}
